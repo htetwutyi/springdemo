5abfdb3d384e175c2865bdc3111f307f712d833f
htetwutyi <htetwutyi.sd@gmail.com>
2020-08-28 15:20:26 +0630

    First Commit

---
+++ src/main/java/com/gcloud/demov2/HomeController.java
@@ -0,0 +1,14 @@
+package com.gcloud.demov2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Welcom demov2 ";
+	}
+
+}
